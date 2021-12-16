112d35a7b40c46b7004dc1e23e3d8b985d276e6f
李惠 <lihuiyf@seeyon.com>
2021-12-16 15:11:31 +0800

    优化打包配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,13 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
+  extends: ["plugin:vue/essential", "eslint:recommended"],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint",
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    "no-undef": 0,
   },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
-}
+};
--- config/common/lib.js
+++ config/common/lib.js
@@ -1,38 +1,12 @@
 require('shelljs/global');
 
-var inquirer = require('inquirer');
-var chalk = require('chalk');
-var package = require('../../package.json');
-var fs = require('fs');
-
-// 驼峰处理
-function tranformName(str) {
-	var strArr = str.split('-');
-	for (var i = 0; i < strArr.length; i++) {
-		strArr[i] = strArr[i].charAt(0).toUpperCase() + strArr[i].substring(1);
-	}
-	return strArr.join('');
-}
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+const config = require('../../package.json');
 
 let run = function () {
 	// 更新
 	exec(`git pull`);
-	
-	// let files = fs.readdirSync('packages').filter(name => name.indexOf('cloud') > -1);
-	// console.log(chalk.blueBright(`正在编译---packages文件夹\n`));
-	// files.forEach((name) => {
-	//     console.log(chalk.blueBright(`正在编译---${name} -> ${tranformName(name)}`));
-	//     exec(`vue-cli-service build --target lib --formats=umd-min --name ${tranformName(name)} --dest lib/${name} packages/${name}/index.js`)
-	// })
-	
-	// files = fs.readdirSync('packages-plugin').filter(name => name.indexOf('cloud') > -1);
-	// if (files && files.length) {
-	//     console.log(chalk.blueBright(`正在编译---packages-plugin文件夹\n`));
-	//     files.forEach((name) => {
-	//         console.log(chalk.blueBright(`正在编译---${name}->${tranformName(name)}`));
-	//         exec(`vue-cli-service build --target lib --formats=umd-min --name ${tranformName(name)} --dest lib/${name} packages-plugin/${name}/index.js`)
-	//     })
-	// }
 	
 	console.log(chalk.blueBright(`编译组件总包`));
 	// 编译组件总包
@@ -41,8 +15,7 @@ let run = function () {
 	compPublish();
 };
 
-// 获取文件夹名称
-let compPublish = function (componentsJson) {
+let compPublish = function () {
 	inquirer.prompt([{
 		name: 'conform',
 		message: `是否发布工程到仓库？`,
@@ -57,8 +30,8 @@ let compPublish = function (componentsJson) {
 		}]
 	}]).then(function (answers) {
 		if (answers.conform) {
-			console.log(`npm unpublish ${package.name}@${package.version} --force`);
-			exec(`npm unpublish ${package.name}@${package.version} --force`);
+			console.log(`npm unpublish ${config.name}@${config.version} --force`);
+			exec(`npm unpublish ${config.name}@${config.version} --force`);
 			console.log(`npm publish`);
 			exec('npm publish');
 			console.log(chalk.green(`已发布`));
