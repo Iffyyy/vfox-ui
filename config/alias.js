4392bb34bf368e865903ad47a54d44b4c35e8ec6
李惠 <lihuiyf@seeyon.com>
2021-12-17 11:30:30 +0800

    Updates

--- config/alias.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-function resolve (dir) {
-  return path.join(__dirname, '../'+dir)
-}
-module.exports = {
-  '_lib': resolve('lib'),
-  '_src': resolve('src'),
-  '_pkg': resolve('packages')
-}
--- docs/.vuepress/enhanceApp.js
+++
@@ -1,9 +0,0 @@
-export default ({ Vue, options, router, siteData }) => {
-    Vue.mixin({
-        mounted() {
-            import('../../packages/index').then(function (UI) {
-                Vue.use(UI.default)
-            })
-        },
-    })
-}
